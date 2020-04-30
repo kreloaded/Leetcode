90d8ee389b36962b6ecd42a003d496861ed254b5
kreloaded <kiran@ost.com>
2020-04-30 22:44:11 +0530

    Added solution for week 2 day 4

---
+++ 30-day-challenge (1 April - 30 April)/week-1-day-4/Solution.java
@@ -0,0 +1,35 @@
+/*
+
+Move Zeroes
+Solution
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Example:
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Note:
+
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+
+*/
+
+// Java Solution
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int count = 0; 
+        int n = nums.length;
+        
+        for (int i = 0; i < n; i++)  {
+            if (nums[i] != 0)  {
+                nums[count++] = nums[i]; 
+            }    
+        }
+           
+        while (count < n) {
+            nums[count++] = 0;   
+        }
+    }
+}
