7379c5c7ddb17f483760163dd81ad35efbada570
kreloaded <kiran@ost.com>
2020-04-30 22:31:58 +0530

    Added solution for week 5 day 2

---
+++ 30-day-challenge (1 April - 30 April)/week-5-day-2/Solution.java
@@ -0,0 +1,80 @@
+/*
+
+Title: Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree
+
+Given a binary tree where each path going from the root to any leaf form a valid sequence, 
+check if a given string is a valid sequence in such binary tree. 
+
+We get the given string from the concatenation of an array of integers arr and the concatenation 
+of all values of the nodes along a path results in a sequence in the given binary tree.
+
+ 
+
+Example 1:
+
+Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,1,0,1]
+Output: true
+Explanation: 
+The path 0 -> 1 -> 0 -> 1 is a valid sequence (green color in the figure). 
+Other valid sequences are: 
+0 -> 1 -> 1 -> 0 
+0 -> 0 -> 0
+
+
+
+Example 2:
+
+Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,0,1]
+Output: false 
+Explanation: The path 0 -> 0 -> 1 does not exist, therefore it is not even a sequence.
+Example 3:
+
+
+
+Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,1,1]
+Output: false
+Explanation: The path 0 -> 1 -> 1 is a sequence, but it is not a valid sequence.
+ 
+
+Constraints:
+
+1 <= arr.length <= 5000
+0 <= arr[i] <= 9
+Each node's value is between [0 - 9].
+
+*/
+
+// Java Solution
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidSequence(TreeNode root, int[] arr) {
+        return existPath(root, arr, 0);
+    }
+    
+    private boolean existPath(TreeNode root, int arr[], int index) 
+    { 
+        if(root==null) { 
+            return arr.length==0; 
+        } if(index == arr.length - 1 && root.left == null && root.right == null && root.val == arr[index]) {
+            return true;
+        } if(index < arr.length && root.val == arr[index]) {
+            return existPath(root.left, arr, index+1) || existPath(root.right, arr, index+1);
+        }
+        return false;
+    } 
+} 
