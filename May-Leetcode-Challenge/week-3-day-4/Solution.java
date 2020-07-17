5e6404ce2f0defeeae2a5a9ede09c38ff3484622
kreloaded <kiran@ost.com>
2020-07-17 13:57:20 +0530

    Added solution for week 3 day 4

---
+++ May-Leetcode-Challenge/week-3-day-4/Solution.java
@@ -0,0 +1,74 @@
+/*
+
+Title: Permutation in String
+
+Problem Statement: Given two strings s1 and s2, write a function to return true if s2 
+	contains the permutation of s1. In other words, one of the first string's permutations is 
+	the substring of the second string.
+
+ 
+Example 1:
+
+Input: s1 = "ab" s2 = "eidbaooo"
+Output: True
+Explanation: s2 contains one permutation of s1 ("ba").
+
+
+Example 2:
+
+Input:s1= "ab" s2 = "eidboaoo"
+Output: False
+ 
+
+Constraints:
+
+The input strings only contain lower case letters.
+The length of both given strings is in range [1, 10,000].
+
+
+Hide Hint #1  
+	Obviously, brute force will result in TLE. Think of something else.
+Hide Hint #2  
+	How will you check whether one string is a permutation of another string?
+Hide Hint #3  
+	One way is to sort the string and then compare. But, Is there a better way?
+Hide Hint #4  
+	If one string is a permutation of another string then they must one common metric. What is that?
+Hide Hint #5  
+	Both strings must have same character frequencies, if one is permutation of another. Which data structure should be used to store frequencies?
+Hide Hint #6  
+	What about hash table? An array of size 26?
+*/
+
+// Java Solution
+
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int s1Length = s1.length();
+        int s2Length = s2.length();
+        
+        if (s1Length > s2Length) return false;
+        
+        int count[] = new int[26];
+        
+        for (int i=0; i<s1Length; i++) {
+            count[s1.charAt(i) - 'a']++;
+        }
+        
+        for (int i=0; i<s2Length; i++) {
+            count[s2.charAt(i) - 'a']--;
+            
+            if ((i - s1Length) >= 0) count[s2.charAt(i - s1Length) - 'a']++;
+            
+            if (checkAllZeros(count)) return true;
+        }
+        return false;
+    }
+    
+    public boolean checkAllZeros(int[] count) {
+        for (int i=0; i<count.length; i++) {
+            if (count[i] != 0) return false;
+        }
+        return true;
+    }
+}
