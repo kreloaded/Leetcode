aaa34220e3c4a0d8514db01d4964a2f32b3fce5f
kreloaded <kiran@ost.com>
2020-07-24 19:07:10 +0530

    wrong day solution folder fix

--- July-Leetcode-Challenge/week-3-day-4/Solution.java
+++ July-Leetcode-Challenge/week-3-day-4/Solution.java
@@ -1,48 +1,84 @@
 /*
 
-Title: Add Binary
+Title: Course Schedule II
 
-Problem Statement: Given two binary strings, return their sum (also a binary string).
+Problem Statement: There are a total of n courses you have to take, labeled from 0 to n-1.
 
-The input strings are both non-empty and contains only characters 1 or 0.
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, 
+which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, return the ordering 
+of courses you should take to finish all courses.
+
+There may be multiple correct orders, you just need to return one of them. 
+If it is impossible to finish all courses, return an empty array.
 
 Example 1:
 
-Input: a = "11", b = "1"
-Output: "100"
+Input: 2, [[1,0]] 
+Output: [0,1]
+Explanation: There are a total of 2 courses to take. To take course 1 you should have finished   
+             course 0. So the correct course order is [0,1] .
 
 
 Example 2:
 
-Input: a = "1010", b = "1011"
-Output: "10101"
- 
+Input: 4, [[1,0],[2,0],[3,1],[3,2]]
+Output: [0,1,2,3] or [0,2,1,3]
+Explanation: There are a total of 4 courses to take. To take course 3 you should have finished both     
+             courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0. 
+             So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3] .
+Note:
 
-Constraints:
+The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+You may assume that there are no duplicate edges in the input prerequisites.
 
-Each string consists only of '0' or '1' characters.
-1 <= a.length, b.length <= 10^4
-Each string is either "0" or doesn't contain any leading zero.
+   Hint #1  
+This problem is equivalent to finding the topological order in a directed graph. If a cycle exists, no topological ordering exists and therefore it will be impossible to take all courses.
+   
+   Hint #2  
+Topological Sort via DFS - A great video tutorial (21 minutes) on Coursera explaining the basic concepts of Topological Sort.
+   
+   Hint #3  
+Topological sort could also be done via BFS.
 
 */
 
-// Java Solution
+// Java Solution - Topological sort
 
 class Solution {
-    public String addBinary(String a, String b) {
-        StringBuilder sb = new StringBuilder();
-        int i = a.length() - 1, j = b.length() -1, carry = 0;
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        if (numCourses == 0) return null;
+    
+        int indegree[] = new int[numCourses], order[] = new int[numCourses], index = 0;
         
-        while (i >= 0 || j >= 0) {
-            int sum = carry;
-            if (j >= 0) sum += b.charAt(j--) - '0';
-            if (i >= 0) sum += a.charAt(i--) - '0';
-            sb.append(sum % 2);
-            carry = sum / 2;
+        for (int i = 0; i < prerequisites.length; i++)
+        {
+            indegree[prerequisites[i][0]]++;    
         }
-        
-        if (carry != 0) sb.append(carry);
-        
-        return sb.reverse().toString();
+            
+        Queue<Integer> queue = new LinkedList<Integer>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                order[index++] = i;
+                queue.offer(i);
+            }
+        }
+
+    
+        while (!queue.isEmpty()) {
+            int prerequisite = queue.poll();
+            for (int i = 0; i < prerequisites.length; i++)  {
+                if (prerequisites[i][1] == prerequisite) {
+                    indegree[prerequisites[i][0]]--; 
+                    if (indegree[prerequisites[i][0]] == 0) {
+                        order[index++] = prerequisites[i][0];
+                        queue.offer(prerequisites[i][0]);
+                    }
+                } 
+            }
+        }
+
+        return (index == numCourses) ? order : new int[0];
     }
 }
